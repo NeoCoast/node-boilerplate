051a492ad324e1ce53634eed654422ff6eb07c1d
Martin Manitto <manitto23@gmail.com>
2020-04-07 12:52:12 -0300

    Changed basic config

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,7 @@
 exports.validateFields = (requiredFields) => (req, res, next) => {
-  const missingFields = requiredFields.map((requiredField) => !req.body[requiredField] && requiredField).filter((missingField) => missingField);
+  const missingFields = requiredFields.map((requiredField) => (
+    !req.body[requiredField] && requiredField
+  )).filter((missingField) => missingField);
 
   if (missingFields.length > 0) {
     return res.status(400).json({
